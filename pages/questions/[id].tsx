eedc73312ef8c8b234c80f7c20386147e6577aaf
Chiaki203 <42350913+Chiaki203@users.noreply.github.com>
2022-02-14 17:59:11 +0100

    answer tweet

--- pages/questions/[id].tsx
+++ pages/questions/[id].tsx
@@ -86,7 +86,7 @@ export default function QuestionShow() {
     setBody('')
     const now = new Date().getTime()
     setAnswer({
-      id: '',
+      id: answerRef.id,
       uid: user.uid,
       questionId: question.id,
       body,
